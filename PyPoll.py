0f8763fe372c6587cd5187f68696ad48718f065f
BiscuitButter <adamwomer@gmail.com>
2020-10-12 19:12:41 -0400

    PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -14,6 +14,11 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Initialize a total vote counter.
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -22,4 +27,27 @@ with open(file_to_load) as election_data:
 
     # Print the header row.
     headers = next(file_reader)
-    print(headers)
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+        
+        # Print the candidate name from each row
+        
+        candidate_name = row[2]
+
+        # Add the candidate name to the candidate list.
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        
+        candidate_votes[candidate_name] += 1
+
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100 
+        print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
+
+# Print the total votes.
+print(candidate_votes)
+

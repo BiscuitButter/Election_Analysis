13f7269f5a2fe6f1f0c87f20f78704e1ebcc736e
BiscuitButter <adamwomer@gmail.com>
2020-10-13 17:53:43 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -18,6 +18,10 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 total_votes = 0
 candidate_options = []
 candidate_votes = {}
+# Winning Candidate and Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -46,8 +50,20 @@ with open(file_to_load) as election_data:
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes) / float(total_votes) * 100 
-        print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
 
 # Print the total votes.
-print(candidate_votes)
+winning_candidate_summary = (
+    f"-----------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-----------------------------\n")
+
+print(winning_candidate_summary)
 

92de150df4983235557419dea0f6486ed907031c
BiscuitButter <adamwomer@gmail.com>
2020-10-17 15:37:52 -0400

    PyPoll Challenge

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -80,7 +80,7 @@ with open(file_to_save, "w") as txt_file:
         winning_county = (
             f"\n-------------------------\n"
             f"Largest County Turnout: {max(county_votes, key=county_votes.get)}\n"
-            f"-------------------------\n\n"
+            f"-------------------------\n"
             )
     # 7: Print the county with the largest turnout to the terminal.
     print(winning_county)
@@ -92,7 +92,7 @@ with open(file_to_save, "w") as txt_file:
         votes = candidate_votes.get(candidate_name)
         vote_percentage = float(votes) / float(total_votes) * 100
         candidate_results = (
-            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n\n")
+            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
         # Print each candidate's voter count and percentage to the terminal.
         print(candidate_results)
         #  Save the candidate results to our text file.
